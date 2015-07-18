2deb8dcfeb4f2608d7f14227d34e6dceee593657
Luke <mrlukecaptain@gmail.com>
2015-07-18 10:38:12 +0100

    Done

--- app/src/main/java/com/lukecaptaincode/diceroller/MainMenu.java
+++ app/src/main/java/com/lukecaptaincode/diceroller/MainMenu.java
@@ -29,6 +29,7 @@ public class MainMenu extends ActionBarActivity {
 
     private ProgressBar spinner;
 
+    //Done
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
